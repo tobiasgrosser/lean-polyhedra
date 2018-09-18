8721c3c63893bf88dd5700eb1f5b1a9fd021cb87
Tobias Grosser <tobias@grosser.es>
2018-09-18 22:04:42 +0200

    Shorten proofs for matrix.le_refl and matrix.le_trans
    Thanks to Kenny and Kevin

--- src/polyhedra.lean
+++ src/polyhedra.lean
@@ -15,28 +15,15 @@ instance [partial_order α] : has_le (matrix n m α) :=
 
 protected lemma matrix.le_refl [partial_order α] (A: matrix n m α) :
 A ≤ A :=
-begin
-  assume i: n,
-  assume j: m,
-  refl
-end
+by intros i j; refl
 
 protected lemma matrix.le_trans [partial_order α] (a b c: matrix n m α)
   (h1 : a ≤ b) (h2 : b ≤ c) : a ≤ c :=
-begin
-  assume i: n,
-  assume j: m,
-  have h1l: a i j ≤ b i j, from h1 i j,
-  have h2l: b i j ≤ c i j, from h2 i j,
-  transitivity,
-  apply h1l,
-  apply h2l,
-end
+by intros i j; transitivity; solve_by_elim
 
 protected lemma matrix.le_antisymm [partial_order α] (a b: matrix n m α)
   (h1 : a ≤ b) (h2 : b ≤ a) : a = b :=
 by ext i j; exact le_antisymm (h1 i j) (h2 i j)
-
 
 instance [partial_order α] : partial_order (matrix n m α) :=
 {
