6a78d3d0ee2fff627fa28628fc97f7aa2b1907d4
Tobias Grosser <tobias@grosser.es>
2018-09-18 22:33:59 +0200

    Add a couple more theorems on polyhedra

--- src/polyhedra.lean
+++ src/polyhedra.lean
@@ -1,4 +1,5 @@
 import ring_theory.matrix
+import data.set
 
 variables {α : Type} {n m l : Type} [fintype n] [fintype m]
 
@@ -36,3 +37,51 @@ local infixl ` *ₘ ` : 70 := matrix.mul
 def polyhedron [ordered_ring α] (A : matrix m n α) (b : matrix m unit α) :
   set (matrix n unit α) :=
 { x : matrix n unit α | A *ₘ x ≥ b }
+
+
+def vec (n: Type) (α : Type) [fintype n] :=
+n → α
+
+instance [ordered_ring α] [decidable_rel ((≤) : α → α → Prop)]
+  (A : matrix m n α) (b : matrix m unit α) (x : matrix n unit α) :
+  decidable (x ∈ polyhedron A b) :=
+show decidable (∀i:m, ∀j:unit, b i j ≤ (A *ₘ x) i j),
+  by apply_instance
+
+variables [ordered_ring α] (A B : matrix m n α) (b : matrix m unit α)
+
+protected def matrix.row (A : matrix m n α) (row : m) : matrix unit n α :=
+λ x: unit, λ y: n, (A row) y
+local attribute classical.prop_decidable
+
+lemma polyhedron_rowinE [ordered_ring α]
+        (x: matrix n unit α) (A: matrix m n α) (b: matrix m unit α):
+    x ∈ (polyhedron A b) = ∀ i:m, (matrix.row A i *ₘ x) () () ≥ b i () :=
+
+propext $ iff.intro
+  (assume h: x ∈ (polyhedron A b),
+   assume d: m,
+   show (matrix.row A d *ₘ x) () () ≥ b d (),
+   begin
+     rw polyhedron at h,
+     rw set.mem_set_of_eq at h,
+     exact h d ()
+   end
+  )
+  (assume h: ∀ i:m, (matrix.row A i *ₘ x) () () ≥ b i (),
+   show x ∈ (polyhedron A b),
+   begin
+   assume d : m,
+   assume j : unit,
+   rw <-(≥),
+   cases j,
+   apply h,
+   end
+  )
+
+def active_ineq [ordered_ring α] (x: matrix n unit α)
+    (A: matrix m n α) (b: matrix m unit α) : set m :=
+{ i | ((A *ₘ x) i () == b i ())}
+
+def feasible_dir [ordered_ring α] (A: matrix m n α) :=
+{ d : matrix n unit α | ((A *ₘ d) ≥ 0)}
